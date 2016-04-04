1f5308b9158536b71e83b1b3ef372af7ea2ffdc9
Alexander Ose <alex@alexose.com>
2016-04-04 14:26:35 -0400

    set up crossfading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $.fn.zeitgeist = function(){
-  this.find('img').each(function(d){
-    console.log(d);  
+  this.find('img:not(.reserve)').each(function(i, d){
+
+    // Append backing image for crossfading
+    var wrapper = $(d)
+      .addClass('front')
+      .wrap('<div class="wrapper" />')
+      .parent();
+
+    wrapper.append('<img class="back hidden" />');
+
+    // Fade in randomly
+    setTimeout(function(){
+      $(d).addClass('visible');
+    }, Math.random() * 1000);
+
   });
 };
 
