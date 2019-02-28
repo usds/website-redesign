91d9cc82bf3285e66477a7b322eeb0ef06472e4c
Jared Cunha <jcunha@omb.eop.gov>
2019-02-28 09:58:26 -0500

    improve instagram alt text

--- assets/js/site/instagram.js
+++ assets/js/site/instagram.js
@@ -12,7 +12,7 @@ var token = '7125099797.0ea7069.7e845ba0cc98481cbabbb8f7f2860444',
       success: function(data){
          console.log(data);
         for( x in data.data ){
-          $('#instagram-feed').append('<li class="site-c-instagallery__item"><a href="'+ data.data[x].link +'" target="_blank" class="site-c-instagallery__link"><img src="'+data.data[x].images.standard_resolution.url+'" class="site-c-instagallery__image" alt="Instagram image"></a></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+          $('#instagram-feed').append('<li class="site-c-instagallery__item"><a href="'+ data.data[x].link +'" target="_blank" class="site-c-instagallery__link"><img src="'+data.data[x].images.standard_resolution.url+'" class="site-c-instagallery__image" alt="'+ data.data[x].caption.text +'"></a></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
           // data.data[x].images.thumbnail.url - URL of image 150х150
           // data.data[x].images.standard_resolution.url - URL of image 612х612
           // data.data[x].link - Instagram post URL
