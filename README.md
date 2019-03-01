9bd8d4b95896caa6f8b6ebefeaa89acf7c07d5f1
Jared Cunha <jcunha@omb.eop.gov>
2019-03-01 12:47:58 -0500

    update package.json and clean gulp functions
    update package and css overrides/changes
    
    sort of working, swithching variables
    
    update breakpoints
    
    remove test accordion
    
    updating css
    
    remove uneccesary variables
    
    create task to update without overwriting theme files
    
    clean old gulp files
    
    reorganize uswds src files, remove uneccessary files
    
    update gulpfile and scss
    
    package.json update

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 # USDS website
 
-## To run
-
-First install packages:
-
-```
-$ npm install --save-dev
-```
-
-Next, run Jekyll
-```
-$ bundle exec jekyll serve
-```
-
 ## Uploading images
 - [/images](https://github.com/usds/website/tree/master/images): This folder contains *editorial photos* and other images for People cards and pages, Project cards and pages, event logos for the Events cards and page, and other areas that are likely to change semi-frequently.
 - [/assets/img](https://github.com/usds/website/tree/master/assets/img): Site assets and evergreen images such as page banners/headers and vector graphics should be placed in the assets/img folder here. The images in this folder typically will not be subject to change.
@@ -20,3 +7,53 @@ $ bundle exec jekyll serve
 ## Adding Content
 * [How to add people](https://github.com/usds/website/wiki/Adding-People-(carousel-and-pages))
 * [How to add projects](https://github.com/usds/website/wiki/Adding-projects-(carousel-and-pages))
+
+
+## To run
+
+```
+$ npm install
+$ bundle exec jekyll serve
+```
+
+### Updating USWDS to a later version.
+
+**Before you do this**: The USWS Sass uses many of the USWDS functions and tokens. While the USWDS.css is already compiled, the site relies on Jekyll to compile the Sass files. There is currently an issue with Jekyll the way it handles `.5` spacing tokens. [See here](https://github.com/uswds/uswds-sandbox/issues/18)
+
+There are a couple ways to update the USWDS:
+
+#### Only update the USDS.css
+
+1. Update the USWDS version in `package.json`
+2. Install the package `npm install`
+2. Compile usds.css `gulp build-sass`
+3. Run Jekylll `bundle exec jekyll serve`
+
+#### Update USDS and get new functions and tokens (not currently recommended)
+
+This will will update some of the scss files in `assets/uswds-sass`.
+
+1. Update the USWDS version in `package.json`
+2. Install the package `npm install`
+3. Compile usds.css `gulp build-sass`
+4. Run `gulp update`
+5. Run Jekyll `bundle exec jekyll serve`
+
+You will likely encounter issues when you run Jekyll. If you get errors, it will likely be that `.5 is not a valid spacing token`. If you get this error, you will need to update each instance in `assets/uswds-sass` of `0.5` as spacing token to `05`.
+
+#### Update USDS, get new functions and tokens, update theme configuration Sass (not recommended)
+
+This will will update some of the scss files in `assets/uswds-sass` and overwrite files in `assets/uswds-theme`.
+
+1. Update the USWDS version in `package.json`
+2. Install the package `npm install`
+3. Compile usds.css `gulp build-sass`
+4. Run `gulp init`
+5. Run Jekyll `bundle exec jekyll serve`
+
+You will likely encounter issues when you run Jekyll. If you get errors, it will likely be that `.5 is not a valid spacing token`. If you get this error, you will need to update each instance in `assets/uswds-sass` of `0.5` as spacing token to `05`.
+
+The only change in the theme is setting utilities to use the `!important` flag to true, which you will want to do before you run step 3.
+```
+$utilities-use-important:     true;
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,158 @@
-// Bring in individual Gulp configurations
-//
+/*
+* * * * * ==============================
+* * * * * ==============================
+* * * * * ==============================
+* * * * * ==============================
+========================================
+========================================
+========================================
+----------------------------------------
+USWDS SASS GULPFILE
+----------------------------------------
+*/
 
-require( './config/gulp/flags' );
-require( './config/gulp/fonts' );
-require( './config/gulp/images' );
-require( './config/gulp/javascript' );
-require( './config/gulp/sass' );
-require( './config/gulp/build' );
+var autoprefixer  = require('autoprefixer');
+var autoprefixerOptions = require('./node_modules/uswds-gulp/config/browsers');
+var cssnano       = require('cssnano');
+var gulp          = require('gulp');
+var mqpacker      = require('css-mqpacker');
+var notify        = require('gulp-notify');
+var path          = require('path');
+var pkg           = require('./node_modules/uswds/package.json');
+var postcss       = require('gulp-postcss');
+var rename        = require('gulp-rename');
+var replace       = require('gulp-replace');
+var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var uswds         = require('./node_modules/uswds-gulp/config/uswds');
 
-var gulp  = require( 'gulp' );
-var dutil = require( './config/gulp/doc-util' );
+/*
+----------------------------------------
+PATHS
+----------------------------------------
+- All paths are relative to the
+  project root
+- Don't use a trailing `/` for path
+  names
+----------------------------------------
+*/
 
-gulp.task( 'default', function ( done ) {
+// Project Sass source directory
+const PROJECT_SASS_SRC = './assets/uswds-theme';
 
-  dutil.logIntroduction();
+// Project Sass source directory
+const PROJECT_USWDS_SASS_SRC = './assets/uswds-sass';
 
-  dutil.logHelp(
-    'gulp',
-    'This task will output the currently supported automation tasks. ( e.g. This help message. )'
-  );
+// Images destination
+const IMG_DEST = './assets/img';
 
-  dutil.logHelp(
-    'gulp build',
-    'This task is an alias for running `gulp clean-assets javascript images sass fonts` and is the recommended task to build all assets.'
-  );
+// Fonts destination
+const FONTS_DEST = './assets/fonts';
 
-  dutil.logCommand(
-    'gulp sass',
-    'This task will compile all the Sass files into the assets directory.'
-  );
+// Javascript destination
+const JS_DEST = './assets/js/vendor';
 
-  dutil.logCommand(
-    'gulp javascript',
-    'This task will compile all the JavaScript files into the assets directory.'
-  );
+// Compiled CSS destination
+const CSS_DEST = './assets/uswds';
 
-  dutil.logCommand(
-    'gulp images',
-    'This task will copy all the image files into the assets directory.'
-  );
+/*
+----------------------------------------
+TASKS
+----------------------------------------
+*/
 
-  dutil.logCommand(
-    'gulp fonts',
-    'This task will copy all the font files into the assets directory.'
-  );
+gulp.task('copy-uswds-setup', () => {
+  return gulp.src(`${uswds}/scss/theme/**/**`)
+  .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
+});
 
-  dutil.logCommand(
-    'gulp html',
-    'This task generates the code snippets based off of uswds package html files.'
-  );
+gulp.task('copy-uswds-core', () => {
+  return gulp.src(`${uswds}/scss/core/**/**`)
+  .pipe(gulp.dest(`${PROJECT_USWDS_SASS_SRC}/core`));
+});
 
-  done();
+gulp.task('copy-uswds-lib', () => {
+  return gulp.src(`${uswds}/scss/lib/**/**`)
+  .pipe(gulp.dest(`${PROJECT_USWDS_SASS_SRC}/lib`));
+});
 
-} );
+gulp.task('copy-uswds-settings', () => {
+  return gulp.src(`${uswds}/scss/settings/**/**`)
+  .pipe(gulp.dest(`${PROJECT_USWDS_SASS_SRC}/settings`));
+});
+
+gulp.task('copy-uswds-fonts', () => {
+  return gulp.src(`${uswds}/fonts/**/**`)
+  .pipe(gulp.dest(`${FONTS_DEST}`));
+});
+
+gulp.task('copy-uswds-images', () => {
+  return gulp.src(`${uswds}/img/**/**`)
+  .pipe(gulp.dest(`${IMG_DEST}`));
+});
+
+gulp.task('copy-uswds-js', () => {
+  return gulp.src(`${uswds}/js/**/**`)
+  .pipe(gulp.dest(`${JS_DEST}`));
+});
+
+gulp.task('build-sass', function(done) {
+  var plugins = [
+    // Autoprefix
+    autoprefixer(autoprefixerOptions),
+    // Pack media queries
+    mqpacker({ sort: true }),
+    // Minify
+    cssnano(({ autoprefixer: { browsers: autoprefixerOptions }}))
+  ];
+  return gulp.src([
+      `${PROJECT_SASS_SRC}/*.scss`
+    ])
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(sass({
+        includePaths: [
+          `${PROJECT_SASS_SRC}`,
+          `${uswds}/scss`,
+          `${uswds}/scss/packages`,
+        ]
+      }))
+    .pipe(replace(
+      /\buswds @version\b/g,
+      'based on uswds v' + pkg.version
+    ))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(`${CSS_DEST}`))
+    .pipe(notify({
+      "sound": "Pop" // case sensitive
+    }));
+});
+
+gulp.task('init', gulp.series(
+  'copy-uswds-setup',
+  'copy-uswds-core',
+  'copy-uswds-lib',
+  'copy-uswds-settings',
+  'copy-uswds-fonts',
+  'copy-uswds-images',
+  'copy-uswds-js',
+  'build-sass',
+));
+
+gulp.task('update', gulp.series(
+  'copy-uswds-core',
+  'copy-uswds-lib',
+  'copy-uswds-settings',
+  'copy-uswds-fonts',
+  'copy-uswds-images',
+  'copy-uswds-js',
+  'build-sass',
+));
+
+gulp.task('watch-sass', function () {
+  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
+});
+
+gulp.task('watch', gulp.series('build-sass', 'watch-sass'));
+
+gulp.task('default', gulp.series('watch'));
