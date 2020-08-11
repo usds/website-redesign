b40cca0ade543eb6068cc5f8f19a933243a2ce81
drew-usds <adgardner@pm-c02z63xelvdl.pitc.gov>
2020-08-10 22:48:32 -0400

    fix breaking build

--- assets/js/site/apply.js
+++ assets/js/site/apply.js
@@ -1,28 +1,30 @@
 // Checks height of Salesforce apply form and applies to iframe
 
-var ifr = document.getElementById("applicationFrame");
-var isTestingMode = false;
+$(function () {
+  var ifr = document.getElementById("applicationFrame");
+  var isTestingMode = false;
 
-function resizeApplicationFrame(height){
-  try{
-    var frameResizedHeight = parseInt(height) + 25;
-    if(isTestingMode){
-      console.log(frameResizedHeight);
-    } else{
-      ifr.style.height = frameResizedHeight + "px";
+  function resizeApplicationFrame(height){
+    try{
+      var frameResizedHeight = parseInt(height) + 25;
+      if(isTestingMode){
+        console.log(frameResizedHeight);
+      } else{
+        ifr.style.height = frameResizedHeight + "px";
+      }
+    }
+    catch {
     }
   }
-  catch {
-  }
-}
+});
 
 window.addEventListener('message', function (e) {
-var eventName = e.data[0];
-var data      = e.data[1];
+  var eventName = e.data[0];
+  var data      = e.data[1];
 
-switch (eventName) {
-  case 'applicationFormHeight':
-    resizeApplicationFrame(data);
-    break;
-}
+  switch (eventName) {
+    case 'applicationFormHeight':
+      resizeApplicationFrame(data);
+      break;
+  }
 }, false);
